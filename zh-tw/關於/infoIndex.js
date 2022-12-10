8403360ef31f88e5707d609d3f7ad74bc743325d
CatsLoveTw <105958410+CatsLoveTw@users.noreply.github.com>
2022-12-10 16:03:01 +0800

    補上更新時間 更新版本號

--- zh-tw/關於/infoIndex.js
+++ zh-tw/關於/infoIndex.js
@@ -1,6 +1,6 @@
-let Rdate = "2022/12/9"
-let Rtime = "22:01:23"
-let Version = "1.0.35_pre3"
+let Rdate = "2022/12/10"
+let Rtime = "15:59:21"
+let Version = "1.0.36_pre1"
 
 
 const date = new Date()
